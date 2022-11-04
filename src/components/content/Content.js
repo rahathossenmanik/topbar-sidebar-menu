cf9c21d7ec0ed5c100d5dda60cfd486888ddbe31
Manik Hosen <rahathossenmanik@gmail.com>
2022-11-05 00:04:26 +0600

    Route Fixed

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -15,9 +15,7 @@ const Content = ({ sidebarIsOpen, toggleSidebar }) => (
 				<Route exact path='/Pages' component={() => 'Pages'} />
 				<Route exact path='/faq' component={() => 'FAQ'} />
 				<Route exact path='/contact' component={() => 'Contact'} />
-				<Route exact path='/Home-1' component={() => 'Home-1'} />
-				<Route exact path='/Home-2' component={() => 'Home-2'} />
-				<Route exact path='/Home-3' component={() => 'Home-3'} />
+				<Route exact path='/home' component={() => 'Home'} />
 				<Route exact path='/Page-1' component={() => 'Page-1'} />
 				<Route exact path='/Page-2' component={() => 'Page-2'} />
 				<Route exact path='/page-1' component={() => 'page-1'} />
