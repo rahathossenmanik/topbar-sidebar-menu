a07902b61f3c5b1c7a9ec3461003e150eedab727
Manik Hosen <rahathossenmanik@gmail.com>
2022-11-05 14:53:07 +0600

    Assets Restructured

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import SideBar from './components/sidebar/SideBar';
-import Content from './components/content/Content';
-import './App.css';
+import SideBar from "./components/sidebar/SideBar";
+import Content from "./components/content/Content";
+import "./assets/index.css";
 
 const App = () => {
-	const [sidebarIsOpen, setSidebarOpen] = useState(true);
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [sidebarIsOpen, setSidebarOpen] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-	useEffect(() => {
-		const resizeWindow = () => setWindowWidth(window.innerWidth);
-		window.addEventListener('resize', resizeWindow);
-		return () => window.removeEventListener('resize', resizeWindow);
-	});
+  useEffect(() => {
+    const resizeWindow = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
+  });
 
-	useEffect(() => {
-		windowWidth <= 768 ? setSidebarOpen(false) : setSidebarOpen(true);
-	}, [windowWidth]);
+  useEffect(() => {
+    windowWidth <= 768 ? setSidebarOpen(false) : setSidebarOpen(true);
+  }, [windowWidth]);
 
-	const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
+  const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
 
-	return (
-		<Router>
-			<div className='App wrapper'>
-				<SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
-				<Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App wrapper">
+        <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+        <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
